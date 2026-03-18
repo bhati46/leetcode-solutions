6e48acfa6dea1554a179e9429a3073a8a3196a01
bhati46 <dakshbhati2900@gmail.com>
2026-03-18 09:32:30 +0530

    Implement N-th Tribonacci Number function

---
+++ leetcode/N-th Tribonacci Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a=0
+        b=1
+        c=1
+        if(n==0 ):
+            return n
+        elif(n==1or n==2):
+            return 1
+        else:
+            for i in range(3,n+1):
+                d=a+b+c
+                a,b,c=b,c,d
+        return c
