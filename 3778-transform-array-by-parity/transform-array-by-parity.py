d424965773aa28092558b827ae5f86b71dfe5708
bhati46 <dakshbhati2900@gmail.com>
2026-04-03 15:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (38.81%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.py
+++ 3778-transform-array-by-parity/transform-array-by-parity.py
@@ -3,9 +3,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i]%2==0:
                 nums[i]=0
-            elif nums[i]%2!=0:
+            else:
                 nums[i]=1
-        z = nums.count(0)
-        o = len(nums) - z
 
-        return [0]*z+[1]*o
+        return sorted(nums)
