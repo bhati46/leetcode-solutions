a6240e3cf674e6e464fd8304b67d03f3c5c0e3ec
bhati46 <dakshbhati2900@gmail.com>
2026-06-02 00:39:04 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (97.00%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -3,8 +3,6 @@ class Solution:
         cost.sort(reverse=True)
         y=0
         x=len(cost)
-        if x==1:
-            return cost[0]
         for i in range(0,x,3):
             if i + 1 < x:
                 y += cost[i] + cost[i+1]
