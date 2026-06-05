95bf4527d261f14516c80d061c8d2593e53814d5
bhati46 <dakshbhati2900@gmail.com>
2026-06-05 23:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (29.04%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -3,7 +3,7 @@ class Solution:
         if n <= 0:
             return False
         factors = [2, 3, 5]
-        for factor in factors:
-            while n % factor == 0:
-                n //= factor
+        for i in factors:
+            while n % i == 0:
+                n //= i
         return n == 1
