69f4228047b3f7841b9c3227a7324dd6b6a50eb8
bhati46 <dakshbhati2900@gmail.com>
2026-08-25 11:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.23%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -3,10 +3,9 @@ class Solution:
         def fun(n,a,b,c):
             if n==0:
                 return a
-            elif n==1:
+            if n==1:
                 return b
-            elif n==2:
+            if n==2:
                 return c
-            else:
-                return fun(n-1,b,c,a+b+c)
+            return fun(n-1,b,c,a+b+c)
         return fun(n,0,1,1)
