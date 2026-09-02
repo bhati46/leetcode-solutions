3e01dcae30a1aa88158da4d3098fcc4f1cefc42d
bhati46 <dakshbhati2900@gmail.com>
2026-09-02 16:20:40 +0530

    Time: 243 ms (22.20%) | Memory: 53 MB (19.50%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,15 +1,15 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         n=len(piles)
-        dp=[-1]*(n*n) #n*n because if n=4 then index would be out of index
+        dp = [[-1] * n for _ in range(n)]
         def fun(l,r):
-            if l==r:
-                return piles[l]
-            index = l * n + r        # at this position 
-            if dp[index] != -1:
-                return dp[index]
-            al=piles[l]-fun(l+1,r)
-            bo=piles[r]-fun(l,r-1)
-            dp[index]=max(al,bo)
-            return dp[index]
-        return fun(0,n-1)>0         
+            if l>r:
+                return 0
+            if dp[l][r] !=-1:
+                return dp[l][r]
+            left=piles[l]-fun(l+1,r)
+            right=piles[r]-fun(l,r-1)
+            dp[l][r]=max(left,right)
+            return dp[l][r]
+        return fun(0,n-1)>0
+            
