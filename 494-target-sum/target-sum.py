1493140e155c1fdc86d04ec010dfb8440407c6e7
bhati46 <dakshbhati2900@gmail.com>
2026-09-16 10:38:44 +0530

    Time: 154 ms (41.38%) | Memory: 42.2 MB (39.50%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTargetSumWays(self, nums: list[int], target: int) -> int:
+        n=len(nums)
+        total=sum(nums)
+        dp=[[-1 for _ in range(2*total+2)] for _ in range(n+2)]
+        def memo(i,cursum):
+            if i==0:
+                return 1 if cursum == target else 0
+            index=cursum+total
+            if dp[i][index]!=-1:
+                return dp[i][index]
+            minus=memo(i-1,cursum-nums[i-1])
+            plus=memo(i-1,cursum+nums[i-1])
+            dp[i][index]=minus+plus
+            return dp[i][index]
+        return memo(n,0)
