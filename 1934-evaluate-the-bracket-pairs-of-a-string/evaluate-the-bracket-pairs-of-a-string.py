04abc03c460f6167e2d6ca12b92f618028ff719d
bhati46 <dakshbhati2900@gmail.com>
2026-09-26 13:37:10 +0530

    Time: 47 ms (69.16%) | Memory: 51.6 MB (39.25%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
+        d=dict(knowledge)
+        y=""
+        i=0
+        while i<len(s):
+            if s[i]=="(": # checking if the sting has ( in s 
+                key=""
+                j=i+1
+                while s[j] !=")":# it will work till the  ) has ended 
+                    key+=s[j]
+                    j+=1
+                if key in d: 
+                    y+=d[key] # this will add the value of key if is in the dict
+                else:
+                    y+="?" # if key is not in dict so we add ?
+                i=j # this is done because it should start iteration where j is ending so it does not go back 
+            else:
+                y+=s[i] # for other char in th str that are not in bracket 
+            i+=1
+        return y
